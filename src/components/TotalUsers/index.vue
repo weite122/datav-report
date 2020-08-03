4b3f1fcec5c22b82e675ec8384004448e10ac17b
weite122 <weite122@foxmail.com>
2020-08-03 22:51:51 +0800

    feat: 用vue-echart重构图标

--- src/components/TotalUsers/index.vue
+++ src/components/TotalUsers/index.vue
@@ -4,7 +4,7 @@
     value="1,087,898"
   >
     <template>
-      <div id="total-users-chart" :style="{width: '100%', height: '100%'}"/>
+      <v-chart :options="getOptions()"/>
     </template>
 
     <template v-slot:footer>
@@ -26,83 +26,84 @@
 
   export default {
     mixins: [commonCardMixin],
-    mounted() {
-      const chartDom = document.getElementById('total-users-chart')
-      const chart = this.$echarts.init(chartDom)
-      chart.setOption({
-        xAxis: {
-          type: 'value',
-          show: false,
-          // boundaryGap: false
-        },
-        yAxis: {
-          type: 'category',
-          show: false
-        },
-        series: [{
-          type: 'bar',
-          stack: '总量',
-          data: [200],
-          barWidth: 10,
-          itemStyle: {
-            color: '#45c946'
+    methods: {
+      getOptions() {
+        return {
+          xAxis: {
+            type: 'value',
+            show: false,
+            // boundaryGap: false
           },
-          // smooth: true
-        }, {
-          type: 'bar',
-          stack: '总量',
-          data: [250],
-          itemStyle: {
-            color: '#eee'
+          yAxis: {
+            type: 'category',
+            show: false
           },
-        }, {
-          type: 'custom',
-          stack: '总量',
-          data: [200],
-          renderItem: (params, api) => {
-            const value = api.value(0)
-            const endPoint = api.coord([value, 0])
-            return {
-              type: 'group',
-              position: endPoint,
-              children: [{
-                type: 'path',
-                shape: {
-                  d: 'M1024 255.996 511.971 767.909 0 255.996 1024 255.996z',
-                  x: -5,
-                  y: -20,
-                  width: 10,
-                  height: 10,
-                  layout: 'cover'
-                },
-                style: {
-                  fill: '#45c946'
-                }
-              }, {
-                type: 'path',
-                shape: {
-                  d: 'M0 767.909l512.029-511.913L1024 767.909 0 767.909z',
-                  x: -5,
-                  y: 10,
-                  width: 10,
-                  height: 10,
-                  layout: 'cover'
-                },
-                style: {
-                  fill: '#45c946'
-                }
-              }]
+          series: [{
+            type: 'bar',
+            stack: '总量',
+            data: [200],
+            barWidth: 10,
+            itemStyle: {
+              color: '#45c946'
+            },
+            // smooth: true
+          }, {
+            type: 'bar',
+            stack: '总量',
+            data: [250],
+            itemStyle: {
+              color: '#eee'
+            },
+          }, {
+            type: 'custom',
+            stack: '总量',
+            data: [200],
+            renderItem: (params, api) => {
+              const value = api.value(0)
+              const endPoint = api.coord([value, 0])
+              return {
+                type: 'group',
+                position: endPoint,
+                children: [{
+                  type: 'path',
+                  shape: {
+                    d: 'M1024 255.996 511.971 767.909 0 255.996 1024 255.996z',
+                    x: -5,
+                    y: -20,
+                    width: 10,
+                    height: 10,
+                    layout: 'cover'
+                  },
+                  style: {
+                    fill: '#45c946'
+                  }
+                }, {
+                  type: 'path',
+                  shape: {
+                    d: 'M0 767.909l512.029-511.913L1024 767.909 0 767.909z',
+                    x: -5,
+                    y: 10,
+                    width: 10,
+                    height: 10,
+                    layout: 'cover'
+                  },
+                  style: {
+                    fill: '#45c946'
+                  }
+                }]
+              }
             }
+          }],
+          grid: {
+            top: 0,
+            bottom: 0,
+            right: 0,
+            left: 0
           }
-        }],
-        grid: {
-          top: 0,
-          bottom: 0,
-          right: 0,
-          left: 0
         }
-      })
-    }
+      }
+    },
+
   }
 </script>
 
