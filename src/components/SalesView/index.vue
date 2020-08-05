7b6f5283eaf2eaa67dab06fb254c6dac7e7e971a
weite122 <weite122@foxmail.com>
2020-08-05 17:39:27 +0800

    feat: SalesView 头部部分完成

--- src/components/SalesView/index.vue
+++ src/components/SalesView/index.vue
@@ -1,15 +1,118 @@
 <template>
-  <div>
-    sales
+  <div class="sales-view">
+    <el-card shadow="hover">
+      <template v-slot:header>
+        <div class="menu-wrapper">
+          <el-menu
+            :default-active="activeIndex"
+            mode="horizontal"
+            @select="onMenuSelect"
+            class="sales-view-el-menu"
+          >
+            <el-menu-item index="1">销售额</el-menu-item>
+            <el-menu-item index="2">访问量</el-menu-item>
+          </el-menu>
+          <div class="menu-right">
+            <el-radio-group v-model="radioSelect" size="small">
+              <el-radio-button label="今日"></el-radio-button>
+              <el-radio-button label="本周"></el-radio-button>
+              <el-radio-button label="本月"></el-radio-button>
+              <el-radio-button label="今年"></el-radio-button>
+            </el-radio-group>
+            <el-date-picker
+              type="daterange"
+              v-model="date"
+              range-separator="至"
+              start-placeholder="开始日期"
+              end-placeholder="结束日期"
+              size="small"
+              :picker-options="pickerOptions"
+              class="sales-view-date-picker"
+            />
+          </div>
+        </div>
+      </template>
+      <template>
+        <div>
+          ddd
+        </div>
+      </template>
+    </el-card>
   </div>
 </template>
 
 <script>
   export default {
-    name: "SalesView"
+    name: "SalesView",
+    data() {
+      return {
+        activeIndex: '1',
+        radioSelect: '今日',
+        date: null,
+        pickerOptions: {
+          shortcuts: [{
+            text: '最近一周',
+            onClick(picker) {
+              const start = new Date()
+              const end = new Date()
+              start.setTime(start.getTime() - 3600 * 24 * 7)
+              picker.$emit('pick', [start, end])
+            }
+          }, {
+            text: '最近一个月',
+            onClick(picker) {
+              const start = new Date()
+              const end = new Date()
+              start.setTime(start.getTime() - 3600 * 24 * 30)
+              picker.$emit('pick', [start, end])
+            }
+          }, {
+            text: '最近三个月',
+            onClick(picker) {
+              const start = new Date()
+              const end = new Date()
+              start.setTime(start.getTime() - 3600 * 24 * 90)
+              picker.$emit('pick', [start, end])
+            }
+          }]
+        }
+      }
+    },
+    methods: {
+      onMenuSelect(index) {
+        this.activeIndex = index
+      }
+    }
   }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+  .sales-view {
+    margin-top: 20px;
+    .menu-wrapper {
+      .sales-view-el-menu {
+        width: 100%;
+        padding-left: 20px;
+        .el-menu-item {
+          height: 50px;
+          line-height: 50px;
+          margin: 0 20px;
+        }
+      }
+      display: flex;
+      position: relative;
+      .menu-right {
+        position: absolute;
+        top: 0;
+        right: 20px;
+        height: 50px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        .sales-view-date-picker{
+          margin-left: 20px;
+        }
+      }
+    }
+  }
 </style>
