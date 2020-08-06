192ae1d61e3098cd60e602023ba298137b20c974
weite122 <weite122@foxmail.com>
2020-08-06 23:59:25 +0800

    feat: 完成Saleview

--- src/components/SalesView/index.vue
+++ src/components/SalesView/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="sales-view">
-    <el-card shadow="hover">
+    <el-card shadow="hover" :body-style="{padding: '0 0 20px 0'}">
       <template v-slot:header>
         <div class="menu-wrapper">
           <el-menu
@@ -33,8 +33,19 @@
         </div>
       </template>
       <template>
-        <div>
-          ddd
+        <div class="sales-view-chart-wrapper">
+          <v-chart :options="chartOption"/>
+          <div class="sales-view-list">
+            <div class="sales-view-title">排行榜</div>
+            <div class="list-item-wrapper">
+              <div class="list-item" v-for="item in rankData" :key="item.no">
+                <div :class="['list-item-no', +item.no <=3 ? 'top-no' : '']">{{item.no}}</div>
+                <div class="list-item-name">{{item.name}}</div>
+                <div class="list-item-money">{{item.money}}</div>
+              </div>
+            </div>
+
+          </div>
         </div>
       </template>
     </el-card>
@@ -75,7 +86,62 @@
               picker.$emit('pick', [start, end])
             }
           }]
-        }
+        },
+        chartOption: {
+          title: {
+            text: '年度销售额',
+            textStyle: {
+              fontSize: 12,
+              color: '#666'
+            }
+          },
+          xAxis: {
+            type: 'category',
+            data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '12月', '12月',]
+          },
+          yAxis: {},
+          series: [{
+            type: 'bar',
+            barWidth: '35%',
+            data: [200, 245, 300, 350, 300, 250, 200, 350, 300, 250, 200, 300]
+          }],
+          color: ['#3398DB'],
+          grid: {
+            top: 70,
+
+          }
+        },
+        rankData: [
+          {
+            no: 1,
+            name: '海底捞',
+            money: '325,324'
+          }, {
+            no: 2,
+            name: '海底捞',
+            money: '325,324'
+          }, {
+            no: 3,
+            name: '海底捞',
+            money: '325,324'
+          }, {
+            no: 4,
+            name: '海底捞',
+            money: '325,324'
+          }, {
+            no: 5,
+            name: '海底捞',
+            money: '325,324'
+          }, {
+            no: 6,
+            name: '海底捞',
+            money: '325,324'
+          }, {
+            no: 7,
+            name: '海底捞',
+            money: '325,324'
+          },
+        ]
       }
     },
     methods: {
@@ -109,8 +175,62 @@
         display: flex;
         align-items: center;
         justify-content: flex-end;
-        .sales-view-date-picker{
+        .sales-view-date-picker {
           margin-left: 20px;
+        }
+      }
+    }
+    .sales-view-chart-wrapper {
+      display: flex;
+      height: 270px;
+      .echarts {
+        flex: 0 0 70%;
+        width: 70%;
+        height: 100%;
+      }
+      .sales-view-list {
+        flex: 1;
+        width: 100%;
+        height: 100%;
+        overflow: hidden;
+        .sales-view-title {
+          margin-top: 20px;
+          font-size: 12px;
+          color: #666;
+          font-weight: 500;
+        }
+        .list-item-wrapper {
+          margin-top: 15px;
+          .list-item {
+            display: flex;
+            align-items: center;
+            font-size: 12px;
+            height: 20px;
+            padding: 6px 20px 6px 0;
+
+            .list-item-no {
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              width: 20px;
+              height: 20px;
+              color: #333;
+              &.top-no {
+                background: #000;
+                border-radius: 50%;
+                color: #fff;
+                font-weight: 500;
+              }
+            }
+            .list-item-name {
+              margin-left: 10px;
+              color: #333;
+            }
+            .list-item-money {
+              flex: 1;
+              text-align: right;
+            }
+          }
         }
       }
     }
