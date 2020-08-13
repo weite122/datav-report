8656bb6fcbc505733f2dd0c20ecdd7aa25702e54
weite122 <weite122@foxmail.com>
2020-08-13 23:53:22 +0800

    feat: 引入销售大盘组件

---
+++ src/views/BMap.vue
@@ -0,0 +1,149 @@
+<template>
+  <v-chart :options="options"/>
+</template>
+
+
+<script>
+  import 'echarts/extension/bmap/bmap'
+
+  const testPoint = [{
+    name: '海门',
+    value: [121.15, 31.89, 80]
+  }, {
+    name: '南京',
+    value: [118.78, 32.04, 100]
+  }]
+  export default {
+    name: "BMap",
+    data() {
+      return {
+        options: {}
+      }
+    },
+    mounted() {
+      this.options = {
+        title: {
+          text: '销售数据大盘',
+          subtext: '销售数据统计',
+          sublink: 'https://cn.bing.com/',
+          left: 'center'
+        },
+        bmap: {
+          key: 'qbQAdDCDB95OSngjICCurka26karU5KQ',
+          center: [104.114129, 37.550339],
+          zoom: 5,
+          roam: true,
+          mapStyle: {
+            styleJson: [{
+              'featureType': 'water',
+              'elementType': 'all',
+              'stylers': {
+                'color': '#d1d1d1'
+              }
+            }, {
+              'featureType': 'land',
+              'elementType': 'all',
+              'stylers': {
+                'color': '#f3f3f3'
+              }
+            }, {
+              'featureType': 'railway',
+              'elementType': 'all',
+              'stylers': {
+                'visibility': 'off'
+              }
+            }, {
+              'featureType': 'highway',
+              'elementType': 'all',
+              'stylers': {
+                'color': '#fdfdfd'
+              }
+            }, {
+              'featureType': 'highway',
+              'elementType': 'labels',
+              'stylers': {
+                'visibility': 'off'
+              }
+            }, {
+              'featureType': 'arterial',
+              'elementType': 'geometry',
+              'stylers': {
+                'color': '#fefefe'
+              }
+            }, {
+              'featureType': 'arterial',
+              'elementType': 'geometry.fill',
+              'stylers': {
+                'color': '#fefefe'
+              }
+            }, {
+              'featureType': 'poi',
+              'elementType': 'all',
+              'stylers': {
+                'visibility': 'off'
+              }
+            }, {
+              'featureType': 'green',
+              'elementType': 'all',
+              'stylers': {
+                'visibility': 'off'
+              }
+            }, {
+              'featureType': 'subway',
+              'elementType': 'all',
+              'stylers': {
+                'visibility': 'off'
+              }
+            }, {
+              'featureType': 'manmade',
+              'elementType': 'all',
+              'stylers': {
+                'color': '#d1d1d1'
+              }
+            }, {
+              'featureType': 'local',
+              'elementType': 'all',
+              'stylers': {
+                'color': '#d1d1d1'
+              }
+            }, {
+              'featureType': 'arterial',
+              'elementType': 'labels',
+              'stylers': {
+                'visibility': 'off'
+              }
+            }, {
+              'featureType': 'boundary',
+              'elementType': 'all',
+              'stylers': {
+                'color': '#fefefe'
+              }
+            }, {
+              'featureType': 'building',
+              'elementType': 'all',
+              'stylers': {
+                'color': '#d1d1d1'
+              }
+            }, {
+              'featureType': 'label',
+              'elementType': 'labels.text.fill',
+              'stylers': {
+                'color': '#999999'
+              }
+            }]
+          }
+        },
+        series: [{
+          name: 'sales',
+          type: 'scatter',
+          coordinateSystem: 'bmap',
+          data: testPoint
+        }]
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
